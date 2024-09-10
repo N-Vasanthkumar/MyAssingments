f97f6adac7faa8a667656d9efc453bcb647985ee
N-Vasanthkumar <nvasanthkumar8436@gmail.com>
2024-09-10 20:31:51 +0530

    Add files via upload

---
+++ week1day2/Chrome.java
@@ -0,0 +1,19 @@
+package week1day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version= 91.0f;
+	    String developer= "Google";
+	    boolean isBeta= false;
+	    int releaseYear= 2008;
+	    char shortcutKey= 'c';
+	    
+	    System.out.println(version+" "+developer+" "+isBeta+" "+releaseYear+" "+shortcutKey);
+	    
+
+	}
+
+}
---
+++ week1day2/Firefox.java
@@ -0,0 +1,22 @@
+package week1day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	float browserVersion =100.2f;
+		
+		boolean isVisible = true;
+		
+		int releaseYear =1998;
+		
+		char browserLogo = 'f'; 
+		
+		
+		System.out.println(browserVersion+" "+isVisible+" "+releaseYear+" "+browserLogo);
+		
+	}
+
+}
